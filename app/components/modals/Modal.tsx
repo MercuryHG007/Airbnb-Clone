8fbc0fef7ac9bd77fb537aba5edac48ac4214a07
Harshit Garg <ggharshit1@gmail.com>
2023-04-20 01:20:13 +0530

    Model UI Updated

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -72,9 +72,11 @@ const Modal: React.FC<ModalProps> = ({
         <>
             <div
                 className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 "
+                onClick={handleClose}
             >
                 <div
                     className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto "
+                    onClick={(e) => e.stopPropagation()}
                 >
                     {/* Content */}
                     <div
